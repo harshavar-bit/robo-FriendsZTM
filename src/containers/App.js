cf53eb3af7a88781845e2629dc7363fb7d3ea13e
harsha vardhan <harshavar968@gmail.com>
2021-07-09 08:19:01 +0530

    Cors policy issue hadled with heroku

--- src/containers/App.js
+++ src/containers/App.js
@@ -16,7 +16,7 @@ class App extends Component  {
 
     componentDidMount () {
         //for the sake of updating robots with api
-        fetch('https://jsonplaceholder.typicode.com/users')
+        fetch('https://mysterious-oasis-81751.herokuapp.com/https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
             .then(users => {this.setState({robots : users})})
         // console.log("componentDidMount");
